08ec6a6f14ebf31ec02da98d9a34ccdd997a5c7e
rinpanman <rinpanman06@gmail.com>
2018-07-12 07:01:22 +0900

    [update] get number of commits from xml

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import requests
 import xml.etree.ElementTree as ET
 
-URL = "https://github.com/users/mitohato/contributions"
+URL = "https://github.com/users/nve3pd/contributions"
 response = requests.get(URL)
 root = ET.fromstring(response.text)
-print(root.tag)
+
+commits = 0
+for e in root.getiterator("rect"):
+    if e.get("data-date") in "2018-07-10":
+        commits = e.get("data-count")
+
+print (commits)
