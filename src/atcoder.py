67f1f771072cee005f29f275bd22e89ffc22dbb5
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-13 22:42:13 +0900

    [refactor] docstringとコメント文を書いた

--- src/atcoder.py
+++ src/atcoder.py
@@ -19,17 +19,19 @@ def get_no1(data):
     with open("ac_count.json", "r") as f:
         last_time = json.load(f)  # 前回の結果をload
     for d in data.items():  # i -> ("name", count)
-        if d[1] - last_time[d[0]] > res[1]:
+        if d[1] - last_time[d[0]] > res[1]:  # 前回との差分を比べて1番半端ないか調べる
             res[0] = d[0]  # name
             res[1] = d[1] - last_time[d[0]]  # AC count
     return res
 
 
 def getAC():
+    """ AC数が最も多い人を返す """
     result = dict()
-    r = requests.get(URL)
+    r = requests.get(URL) 
     ac_data = json.loads(r.text)
     for i in filter(lambda x: x["user_id"] in users, ac_data):
+        # usersの中に入ってる人だけを抽出
         result[i["user_id"]] = i["problem_count"]
 
     save(result)  # save ac_count
