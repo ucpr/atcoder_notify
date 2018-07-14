16f374639df6c9366e8933976d5c9c8f1fe9703b
u_chi_ha_ra_ <uchihara1229@gmail.com>
2018-07-14 23:25:01 +0900

    [refactor] usersをグローバルで読み込むようにした

--- src/github.py
+++ src/github.py
@@ -3,6 +3,8 @@ import xml.etree.ElementTree as ET
 from datetime import date, timedelta
 import json
 
+with open('users.json', 'r') as f:
+    users = json.load(f)
 proxies = {
     'http': 'http://cproxy.okinawa-ct.ac.jp:8080',
     'https': 'http://cproxy.okinawa-ct.ac.jp:8080',
@@ -29,23 +31,13 @@ def get_commits_number(root, yesterday):
     return commits
 
 
-def json_read():
-    with open('users.json', 'r') as f:
-        data = json.load(f)
-
-    return data
-
-
 def get_commit():
-    j_data = json_read()
-    users = j_data["users"]["github"]
-
     date = get_date()
     commits = {}
     sorted_value = []
 
     for name in users:
-        URL = 'https://github.com/users/' + name + '/contributions'
+        URL = f'https://github.com/users/{name}/contributions'
         root = get_xml(URL)
         commits[name] = get_commits_number(root, date)
 
